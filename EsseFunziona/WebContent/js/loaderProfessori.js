cc1c1a384c11042f86c6c7621244135fa287bfe7
Matta <Matta@Matta-PC.homenet.telecomitalia.it>
2018-01-07 16:12:09 +0100

    aggiunto takeProfessori

---
+++ EsseFunziona/WebContent/js/loaderProfessori.js
@@ -0,0 +1,18 @@
+
+function caricaStudenti(){
+	var xhr= new XMLHttpRequest();
+	xhr.open('get',"professori",true);
+	xhr.onload=function(){
+		var jsonStringQuotes = xhr.responseText;
+		var professori=JSON.parse(jsonStringQuotes);
+		for(var i in professori){
+			var p = $('<tr>  <th>'+professori[i].nomeUtente+'</th> <th>'+professori[i].nome+'</th> <th>'+professori[i].cognome+'</th> <th>'+professori[i].dataDiNascita+'</th> <th>'+professori.email+'</th>  </tr>');
+			$("#listaProfessori").append(p);
+		}
+	};
+	xhr.send(null)
+}
+
+$(document).ready(function() {
+	caricaStudenti();	
+});
