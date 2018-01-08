6f125bdd11a3da6f1bfe87532bab9fadb42ce674
lucaq <lucaq@DESKTOP-HJM7F7U.station>
2018-01-08 23:10:50 +0100

    miao

--- EsseFunziona/WebContent/js/loaderBandi.js
+++ EsseFunziona/WebContent/js/loaderBandi.js
@@ -1,12 +1,12 @@
 function caricaBandi(){
 	var xhr= new XMLHttpRequest();
-	xhr.open('get',"documentiCorsi",true);
+	xhr.open('get',"bandiNews",true);
 	xhr.onload=function(){
 		var jsonStringQuotes = xhr.responseText;
 		var bandi=JSON.parse(jsonStringQuotes);
 		for(var i in bandi){
 			var p = $('<tr>  <th>'+bandi[i].id+'</th> <th>'+bandi[i].contenuto+'</th>  <th>'+bandi[i].professore+'</th> </tr>');
-			$("#listaDocumenti").append(p);
+			$("#listaBandi").append(p);
 		}
 	};
 	xhr.send(null)
