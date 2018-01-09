79c68671af79d8f68c7d26b72c9c40ff2affd9ff
lucaq <lucaq@DESKTOP-HJM7F7U.station>
2018-01-09 13:58:54 +0100

    miao

--- EsseFunziona/src/controller/showData.java
+++ EsseFunziona/src/controller/showData.java
@@ -1,29 +1,33 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import persistence.DatabaseManager;
 public class showData extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher dispacher = 
-				req.getRequestDispatcher(".jsp");
-		dispacher.forward(req, resp);
 	}
 	
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
-		RequestDispatcher dispacher = req.getRequestDispatcher(".jsp");
-		dispacher.forward(req, resp);
-	
+		HttpSession session = req.getSession();
+		PrintWriter out = resp.getWriter();
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		StudenteDao dao = DatabaseManager.getInstance().getDaoFactory().getStudenteDAO();
+		StudenteCredenziali studente = dao.findByPrimaryKeyCredential(username);
+		
+		
 	}
 }
