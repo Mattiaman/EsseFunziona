0e70f3b34d8333327e839097cef9393863a0a526
lucaq <lucaq@DESKTOP-HJM7F7U.station>
2018-01-09 12:03:14 +0100

    miagolando

---
+++ EsseFunziona/src/controller/addEsito.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class addEsito extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispacher = 
+				req.getRequestDispatcher(".jsp");
+		dispacher.forward(req, resp);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		RequestDispatcher dispacher = req.getRequestDispatcher(".jsp");
+		dispacher.forward(req, resp);
+	
+	}
+}
