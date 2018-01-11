99ab64b4a178e7ebab615d0ed597dcdd7669b243
Matta <Matta@Matta-PC>
2018-01-11 12:20:25 +0100

    aaa

--- EsseFunziona/WebContent/js/loaderCorsi.js
+++ EsseFunziona/WebContent/js/loaderCorsi.js
@@ -1,18 +1,38 @@
+function caricaProfessori() {
 
-function caricaProfessori(){
-	var xhr= new XMLHttpRequest();
-	xhr.open('get',"corsi",true);
-	xhr.onload=function(){
-		var jsonStringQuotes = xhr.responseText;
-		var corsi=JSON.parse(jsonStringQuotes);
-		for(var i in corsi){
-			var c = $('<tr>  <th>'+corsi[i].id+'</th> <th>'+corsi[i].nome+'</th>  </tr>');
-			$("#listaCorsi").append(c);
-		}
-	};
+	var xhr = new XMLHttpRequest();
+	xhr.open('get', "corsi", true);
+	xhr.onload = function() {
+
+		var xhrA = new XMLHttpRequest();
+		xhrA.open('get', 'datiAnagrafici');
+		xhrA.onload = function() {
+			var jsonStringQuotesA = xhr.responseText;
+			var datiAnagrafici = JSON.parse(jsonStringQuotesA);
+			var jsonStringQuotes = xhr.responseText;
+			var corsi = JSON.parse(jsonStringQuotes);
+			if(datiAnagrafici.tipo=="admin"){
+				$('thead').find('tr').append('<th>Elimina<th>')
+				console.log('ok')
+			}
+			console.log(datiAnagrafici.tipo)
+			for ( var i in corsi) {
+				var c;
+				if(datiAnagrafici.tipo!="admin")
+					c = $('<tr>  <th>' + corsi[i].id + '</th> <th>'
+						+ corsi[i].nome + '</th>  </tr>');
+				else
+					c = $('<tr>  <th>' + corsi[i].id + '</th> <th>'
+							+ corsi[i].nome + '</th>  </tr>');
+					
+				$("#listaCorsi").append(c);
+			}
+		};
+		xhrA.send(null);
+	}
 	xhr.send(null)
 }
 
 $(document).ready(function() {
-	caricaProfessori();	
+	caricaProfessori();
 });
