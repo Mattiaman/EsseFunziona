856a047a9965846ab6009f26f8a127aa6f4280db
Matta <Matta@Matta-PC.homenet.telecomitalia.it>
2018-01-16 19:16:57 +0100

    fatto materiale

--- EsseFunziona/WebContent/js/loaderDocumenti.js
+++ EsseFunziona/WebContent/js/loaderDocumenti.js
@@ -1,17 +1,21 @@
-function caricaDocumetni(){
-	var xhr= new XMLHttpRequest();
-	xhr.open('get',"documenti",true);
-	xhr.onload=function(){
-		var jsonStringQuotes = xhr.responseText;
-		var documenti=JSON.parse(jsonStringQuotes);
-		for(var i in documenti){
-			var p = $('<tr>  <th>'+documenti[i].id+'</th> <th>'+documenti[i].contenuto+'</th>  <th>'+documenti[i].professore+'</th> </tr>');
-			$("#listaDocumenti").append(p);
+function caricaDocumenti(){
+
+	$.ajax({
+
+		url:"documentiCorsi",
+		
+		type:"GET",
+		
+		success: function(documenti){
+			for(var i in documenti){
+				var p = $('<tr>  <th class=\"id\">'+documenti[i].id+'</th> <th class=\"download\"><a href=\"downloader?id='+documenti[i].id+'&tipo=materiale\">content</a></th>  <th>'+documenti[i].professore.nome+' '+documenti[i].professore.cognome+'</th> </tr>');
+				$("#listaDocumenti").append(p);
+			}
+			//downloadListener()
 		}
-	};
-	xhr.send(null)
+	})
 }
 
 $(document).ready(function() {
-	caricaDocumetni();	
+	caricaDocumenti();	
 });
