452386de61fa5a96a22d7a198077d904bbf585b5
Matta <Matta@Matta-PC.homenet.telecomitalia.it>
2018-01-17 15:14:55 +0100

    fatto

--- EsseFunziona/WebContent/js/loaderBandi.js
+++ EsseFunziona/WebContent/js/loaderBandi.js
@@ -5,7 +5,7 @@ function caricaBandi(){
 		var jsonStringQuotes = xhr.responseText;
 		var bandi=JSON.parse(jsonStringQuotes);
 		for(var i in bandi){
-			var p = $('<tr>  <th>'+bandi[i].id+'</th> <th id=\''+bandi[i].id+'\'>content</th>  <th>'+bandi[i].admin.nome+' '+bandi[i].admin.cognome+'</th> </tr>');
+			var p = $('<tr>  <th>'+bandi[i].id+'</th> <th id=\''+bandi[i].id+'\'><a href=\"downloader?id='+bandi[i].id+'&tipo=bando\">content</a></th>  <th>'+bandi[i].admin.nome+' '+bandi[i].admin.cognome+'</th> </tr>');
 			$("#listaBandi").append(p);
 			$('#'+bandi[i].id+'').on('click', function(){
 				var xhrFile= new XMLHttpRequest();
