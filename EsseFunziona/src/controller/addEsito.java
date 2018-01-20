27b82cbdd0fd9e4c416d55aa337b73c0d969df5d
lucaq <lucaq@DESKTOP-HJM7F7U.station>
2018-01-20 13:33:33 +0100

    controlli finiti

--- EsseFunziona/src/controller/addEsito.java
+++ EsseFunziona/src/controller/addEsito.java
@@ -27,7 +27,7 @@ public class addEsito extends HttpServlet{
 		String idAppello = req.getParameter("appello");
 		String voto = req.getParameter("voto");
 		
-		if (!matricola.isEmpty() && !idAppello.isEmpty() && !voto.isEmpty()) {
+		if(!voto.isEmpty()) {
 			StudenteDAO studenteDAO = DatabaseManager.getInstance().getDaoFactory().getStudenteDAO();
 			Studente studente = studenteDAO.findByPrimaryKey(matricola);
 			AppelloDAO appelloDAO = DatabaseManager.getInstance().getDaoFactory().getAppelloDAO();
